d1ddc8d07ea5f3358dfa235f291ed95ffe3a2f48
Marco Zanoni <marco.zanoni@disco.unimib.it>
2016-05-17 15:57:26 +0200

    Add displaytag-1.2

---
+++ projects/displaytag-1.2/displaytag/src/test/java/org/displaytag/decorator/TableDecoratorTwo.java
@@ -0,0 +1,19 @@
+package org.displaytag.decorator;
+
+/**
+ * Test decorator used in tests.
+ * @author Fabrizio Giustina
+ * @version $Revision: 1081 $ ($Author: fgiust $)
+ */
+class TableDecoratorTwo extends TableDecorator
+{
+
+    /**
+     * getter property for "two".
+     * @return "two"
+     */
+    public String getTwo()
+    {
+        return "two";
+    }
+}
